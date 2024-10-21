227bf9565f2dcafcc9bb529f548bb5ae35456b04
Subir Mansukhani <subir.m@gmail.com>
2024-10-20 17:29:35 -0700

    Add files via upload

--- run.py
+++ run.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
             default_team = Team(name="Default Team")
             db.session.add(default_team)
             db.session.commit()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5001)
